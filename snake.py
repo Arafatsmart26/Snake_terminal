fe65306a35607cc65a83ae52cb1fa2baad5cded8
26th-Ara <araara0002@gmail.com>
2023-02-28 08:49:12 +0100

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,21 @@
+import msvcrt
+
+def a():
+    if input(a
+        return 
+def s():
+    pass
+def d():
+    pass
+def w():
+    pass
+
+num = 0
+done = False
+while not done:
+    print(num)
+    num += 1
+
+    if msvcrt.kbhit():
+        print("you pressed",msvcrt.getch(),"so now i will quit")
+        done = True
