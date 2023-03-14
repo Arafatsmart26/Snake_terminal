d7bab5018d2260919175331445ab7d961aa17ee8
26th-Ara <araara0002@gmail.com>
2023-03-14 13:11:36 +0100

    big

--- snake.py
+++ snake.py
@@ -1,13 +1,15 @@
 import msvcrt
 import random
+import time
+import os
 
 #random
 def random_position():
     return (random.randint(0,20)),(random.randint(0,20))
 
 #Snake-bane
-bredde = 50
-højde = 50
+bredde = 30
+højde = 30
 
 #slange
 slangehoved = (10,10)
@@ -16,17 +18,40 @@ slangekrop = [(11,10)]
 #frugt
 frugt = [random.randint]
 
-#vis bane
+
+        
+        
+
+#vis slange
 
 
 
 
-num = 0
 done = False
+currentTime = 0
+previousTime = 0
+interval = 500
 while not done:
-    print(num)
-    num += 1
+    
+    currentTime = time.perf_counter() * 1000
+    if currentTime - previousTime > interval:
+        os.system("cls ")
+        previousTime = currentTime
+        #vis bane
+        for y in range(højde):
+            print("")
+            for x in range(bredde):
+                if slangehoved[0]== x and slangehoved[1]== y:
+                    print("@", end = " ")
+                elif slangekrop[0][0] == x and slangekrop[0][1]==y:
+                    print ("H",end= " ")
+                else:
+                    print(".", end = " ")
+    
 
     if msvcrt.kbhit():
+        ch = msvcrt.getch()
+        if ch == b'd':
+            
         print("you pressed",msvcrt.getch(),"so now i will quit")
         done = True
